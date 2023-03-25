49e059930ef334fbdc353fd85cd68e91ad59d368
권 도윤 <doyoun8813@i-commtech.com>
2023-03-25 23:57:56 +0900

    @EnableConfigurationProperties를 사용하여 개별로 등록하는 방법 대신 @ConfigurationPropertiesScan를 사용하여 @ConfigurationProperties 애노테이션을 가진 클래스를 빈으로 등록하는 방법을 사용할 수 있다.

--- src/main/java/hello/config/MyDataSourceConfigV1.java
+++ src/main/java/hello/config/MyDataSourceConfigV1.java
@@ -8,7 +8,7 @@ import hello.datasource.MyDataSourcePropertiesV1;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@EnableConfigurationProperties(MyDataSourcePropertiesV1.class)
+//@EnableConfigurationProperties(MyDataSourcePropertiesV1.class)
 public class MyDataSourceConfigV1 {
 
 	private final MyDataSourcePropertiesV1 properties;
